fd6c0f48a1d258ab499a6fe5b303df59374952c3
paokyereagy <p.okyereagy@alustudent.com>
2026-03-29 12:19:36 -0700

    my bash script project 17-tree

--- basics/enter.sh
+++ basics/enter.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-
 read -p "Enter the file name" file
 read -p "Enter the command" com
-
 echo -e "#!/bin/bash \n${com}" > ${file}
 chmod u+x ${file}
-
 git add .
 git commit -m "my bash script project ${file}"
 git push
-
