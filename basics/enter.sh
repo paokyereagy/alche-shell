f7d97221a6326d88ee93e7db2098fdd5a6379617
paokyereagy <p.okyereagy@alustudent.com>
2026-03-29 12:05:05 -0700

    my bash script project

--- basics/enter.sh
+++ basics/enter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-read -p "Enter the file name" file
-read -p "Enter the command" com
+read -p "Enter the file name"file
+read -p "Enter the command"com
 
 echo -e "#!/bin/bash \n${com}"
 chmod u+x ${file}
