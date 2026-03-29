cd1bed07b6c0e88a7b71f91ab9f671b8991e102b
paokyereagy <p.okyereagy@alustudent.com>
2026-03-29 12:07:51 -0700

    my bash script project 13-symbolic_link

--- basics/enter.sh
+++ basics/enter.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-read -p "Enter the file name"file
-read -p "Enter the command"com
+read -p "Enter the file name" file
+read -p "Enter the command" com
 
-echo -e "#!/bin/bash \n${com}"
+echo -e "#!/bin/bash \n${com}" > ${file}
 chmod u+x ${file}
 
 git add .
